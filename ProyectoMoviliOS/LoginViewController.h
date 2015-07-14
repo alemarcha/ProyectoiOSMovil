f5c3a288a1377d5196477d1f287d2da98be9725c
Alexis Martínez <alemarcha26@gmail.com>
2015-07-14 15:13:14 +0200

    Añadida views, y controladores.

---
+++ ProyectoMoviliOS/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  ProyectoMoviliOS
+//
+//  Created by Alexis Martínez on 14/7/15.
+//  Copyright (c) 2015 Alexis Martínez Chacón. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ ProyectoMoviliOS/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  ProyectoMoviliOS
+//
+//  Created by Alexis Martínez on 14/7/15.
+//  Copyright (c) 2015 Alexis Martínez Chacón. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
